eb7820a2e022035cc12e9e3d0130d928434141ae
Juan Carlos Camacho <65825162+juancarloscamacho174@users.noreply.github.com>
2021-01-03 20:11:59 +0100

    ranking funcional

--- src/components/table.js
+++ src/components/table.js
@@ -2,14 +2,43 @@ import React from "react";
 import firebase from "firebase";
 
 export default function Table() {
+  const [sortType, setSortType] = React.useState([]);
   const [spells, setSpells] = React.useState([]);
-
+  const [data, setData] = React.useState([]);
+  /*
+  const spells = [
+    {
+      id: 1,
+      nickname: "Nightwish",
+      coins: 9,
+      coinsEarned: 200,
+      hits: 6,
+      hitStreak: 1996
+    },
+    {
+      id: 2,
+      nickname: "Metallica",
+      coins: 10,
+      coinsEarned: 200,
+      hits: 4,
+      hitStreak: 1981
+    },
+    {
+      id: 3,
+      nickname: "Nirvana",
+      coins: 3,
+      coinsEarned: 200,
+      hits: 3,
+      hitStreak: 1987
+    }
+  ];
+*/
   React.useEffect(() => {
     const fetchData = async () => {
       const data = await firebase
         .firestore()
         .collection("users")
-        .orderBy("hits", "desc")
+        //.orderBy("coins", "desc")
         .get();
 
       setSpells(
@@ -20,6 +49,24 @@ export default function Table() {
     };
     fetchData();
   }, []);
+
+  React.useEffect(() => {
+    const sortArray = (type) => {
+      const types = {
+        coins: "coins",
+        hits: "hits",
+        hitStreak: "hitStreak",
+        coinsEarned: "coinsEarned"
+      };
+      const sortProperty = types[type];
+      const sorted = [...spells].sort(
+        (a, b) => b[sortProperty] - a[sortProperty]
+      );
+      setData(sorted);
+    };
+
+    sortArray(sortType);
+  }, [sortType]);
 
   /*return (
     <ul>
@@ -50,35 +97,43 @@ export default function Table() {
                   </th>
                   <th
                     scope="col"
-                    class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                    class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider cursor-pointer hover:bg-gray-800"
+                    onClick={() => {
+                      setSortType("coins");
+                    }}
                   >
                     Ranking Actual
                   </th>
                   <th
                     scope="col"
-                    class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                    class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider cursor-pointer hover:bg-gray-800"
+                    onClick={() => {
+                      setSortType("coinsEarned");
+                    }}
                   >
                     Ranking acumulado
                   </th>
                   <th
                     scope="col"
-                    class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                    class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider cursor-pointer hover:bg-gray-800"
+                    onClick={() => {
+                      setSortType("hitStreak");
+                    }}
                   >
                     Racha de aciertos
                   </th>
                   <th
                     scope="col"
-                    class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-                    ref={spells}
+                    class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider cursor-pointer hover:bg-gray-800"
                     onClick={() => {
-                      spells.orderByChild("hits");
+                      setSortType("hits");
                     }}
                   >
                     Porcentaje de aciertos
                   </th>
                 </tr>
               </thead>
-              {spells.map((spell) => (
+              {data.map((spell) => (
                 <>
                   <tbody
                     class="bg-gray-500 divide-y divide-gray-200"
--- src/pages/home.js
+++ src/pages/home.js
@@ -9,159 +9,145 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 
 export default function Home() {
-  var user = firebase.auth()
-    .currentUser;
+  var user = firebase.auth().currentUser;
 
   return (
     <>
-      <Navbar />
-      <section class="text-gray-600 body-font">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="text-center mb-20">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">
-              Bienvenido {user.email}!
-            </h1>
-            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
-              Accede a los mercados
-              académicos en cualquier
-              momento, en cualquier
-              lugar. Nosotros te
-              facilitamos las
-              herramientas y las
-              posibilidades para que
-              controles tus créditos
-              académicos.
-            </p>
-            <div class="flex mt-6 justify-center">
-              <div class="w-16 h-1 rounded-full bg-green-500 inline-flex"></div>
-            </div>
-          </div>
-          <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
-            <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-              <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
-                <svg
-                  viewBox="64 64 896 896"
-                  focusable="false"
-                  data-icon="line-chart"
-                  width="3em"
-                  height="3em"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M888 792H200V168c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v688c0 4.4 3.6 8 8 8h752c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM305.8 637.7c3.1 3.1 8.1 3.1 11.3 0l138.3-137.6L583 628.5c3.1 3.1 8.2 3.1 11.3 0l275.4-275.3c3.1-3.1 3.1-8.2 0-11.3l-39.6-39.6a8.03 8.03 0 00-11.3 0l-230 229.9L461.4 404a8.03 8.03 0 00-11.3 0L266.3 586.7a8.03 8.03 0 000 11.3l39.5 39.7z"></path>
-                </svg>
-              </div>
-              <div class="flex-grow">
-                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
-                  Apuestas
-                </h2>
-                <p class="leading-relaxed text-base">
-                  Creemos que mereces
-                  más PinfCoins de los
-                  que tienes. <br />
-                  No te quedes parado,
-                  empieza a apostar!
+      <div className="flex flex-col h-screen">
+        <Navbar />
+        <div className="mb-auto">
+          <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+              <div class="text-center mb-20">
+                <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">
+                  Bienvenido {user.email}!
+                </h1>
+                <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
+                  Accede a los mercados académicos en cualquier momento, en
+                  cualquier lugar. Nosotros te facilitamos las herramientas y
+                  las posibilidades para que controles tus créditos académicos.
                 </p>
-                <Link
-                  className="mt-9 text-green-500 inline-flex items-center"
-                  to="/bets"
-                >
-                  Apostar
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </Link>
+                <div class="flex mt-6 justify-center">
+                  <div class="w-16 h-1 rounded-full bg-green-500 inline-flex"></div>
+                </div>
+              </div>
+              <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+                <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                  <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
+                    <svg
+                      viewBox="64 64 896 896"
+                      focusable="false"
+                      data-icon="line-chart"
+                      width="3em"
+                      height="3em"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M888 792H200V168c0-4.4-3.6-8-8-8h-56c-4.4 0-8 3.6-8 8v688c0 4.4 3.6 8 8 8h752c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM305.8 637.7c3.1 3.1 8.1 3.1 11.3 0l138.3-137.6L583 628.5c3.1 3.1 8.2 3.1 11.3 0l275.4-275.3c3.1-3.1 3.1-8.2 0-11.3l-39.6-39.6a8.03 8.03 0 00-11.3 0l-230 229.9L461.4 404a8.03 8.03 0 00-11.3 0L266.3 586.7a8.03 8.03 0 000 11.3l39.5 39.7z"></path>
+                    </svg>
+                  </div>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                      Apuestas
+                    </h2>
+                    <p class="leading-relaxed text-base">
+                      Creemos que mereces más PinfCoins de los que tienes.{" "}
+                      <br />
+                      No te quedes parado, empieza a apostar!
+                    </p>
+                    <Link
+                      className="mt-9 text-green-500 inline-flex items-center"
+                      to="/bets"
+                    >
+                      Apostar
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </Link>
+                  </div>
+                </div>
+                <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                  <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
+                    <svg
+                      viewBox="64 64 896 896"
+                      focusable="false"
+                      data-icon="lock"
+                      width="3em"
+                      height="3em"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M832 464h-68V240c0-70.7-57.3-128-128-128H388c-70.7 0-128 57.3-128 128v224h-68c-17.7 0-32 14.3-32 32v384c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V496c0-17.7-14.3-32-32-32zM332 240c0-30.9 25.1-56 56-56h248c30.9 0 56 25.1 56 56v224H332V240zm460 600H232V536h560v304zM484 701v53c0 4.4 3.6 8 8 8h40c4.4 0 8-3.6 8-8v-53a48.01 48.01 0 10-56 0z"></path>
+                    </svg>
+                  </div>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                      Seguridad
+                    </h2>
+                    <p class="leading-relaxed text-base">
+                      No temas por tu seguridad, esto es un lugar seguro. Todos
+                      los movimientos que realices en esta plataforma son
+                      privados. Además puedes revisar cómo son tratados tus
+                      datos.
+                    </p>
+                    <Link
+                      className="mt-3 text-green-500 inline-flex items-center"
+                      to="/legalterms"
+                    >
+                      Términos legales
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </Link>
+                  </div>
+                </div>
+                <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+                  <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
+                    <svg
+                      viewBox="64 64 896 896"
+                      focusable="false"
+                      data-icon="team"
+                      width="3em"
+                      height="3em"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M824.2 699.9a301.55 301.55 0 00-86.4-60.4C783.1 602.8 812 546.8 812 484c0-110.8-92.4-201.7-203.2-200-109.1 1.7-197 90.6-197 200 0 62.8 29 118.8 74.2 155.5a300.95 300.95 0 00-86.4 60.4C345 754.6 314 826.8 312 903.8a8 8 0 008 8.2h56c4.3 0 7.9-3.4 8-7.7 1.9-58 25.4-112.3 66.7-153.5A226.62 226.62 0 01612 684c60.9 0 118.2 23.7 161.3 66.8C814.5 792 838 846.3 840 904.3c.1 4.3 3.7 7.7 8 7.7h56a8 8 0 008-8.2c-2-77-33-149.2-87.8-203.9zM612 612c-34.2 0-66.4-13.3-90.5-37.5a126.86 126.86 0 01-37.5-91.8c.3-32.8 13.4-64.5 36.3-88 24-24.6 56.1-38.3 90.4-38.7 33.9-.3 66.8 12.9 91 36.6 24.8 24.3 38.4 56.8 38.4 91.4 0 34.2-13.3 66.3-37.5 90.5A127.3 127.3 0 01612 612zM361.5 510.4c-.9-8.7-1.4-17.5-1.4-26.4 0-15.9 1.5-31.4 4.3-46.5.7-3.6-1.2-7.3-4.5-8.8-13.6-6.1-26.1-14.5-36.9-25.1a127.54 127.54 0 01-38.7-95.4c.9-32.1 13.8-62.6 36.3-85.6 24.7-25.3 57.9-39.1 93.2-38.7 31.9.3 62.7 12.6 86 34.4 7.9 7.4 14.7 15.6 20.4 24.4 2 3.1 5.9 4.4 9.3 3.2 17.6-6.1 36.2-10.4 55.3-12.4 5.6-.6 8.8-6.6 6.3-11.6-32.5-64.3-98.9-108.7-175.7-109.9-110.9-1.7-203.3 89.2-203.3 199.9 0 62.8 28.9 118.8 74.2 155.5-31.8 14.7-61.1 35-86.5 60.4-54.8 54.7-85.8 126.9-87.8 204a8 8 0 008 8.2h56.1c4.3 0 7.9-3.4 8-7.7 1.9-58 25.4-112.3 66.7-153.5 29.4-29.4 65.4-49.8 104.7-59.7 3.9-1 6.5-4.7 6-8.7z"></path>
+                    </svg>
+                  </div>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                      Social
+                    </h2>
+                    <p class="leading-relaxed text-base">
+                      Conéctate todos los días a la plataforma para poder
+                      charlar con tus amigos y debatir la siguiente apuesta
+                      ganadora!
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-              <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
-                <svg
-                  viewBox="64 64 896 896"
-                  focusable="false"
-                  data-icon="lock"
-                  width="3em"
-                  height="3em"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M832 464h-68V240c0-70.7-57.3-128-128-128H388c-70.7 0-128 57.3-128 128v224h-68c-17.7 0-32 14.3-32 32v384c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V496c0-17.7-14.3-32-32-32zM332 240c0-30.9 25.1-56 56-56h248c30.9 0 56 25.1 56 56v224H332V240zm460 600H232V536h560v304zM484 701v53c0 4.4 3.6 8 8 8h40c4.4 0 8-3.6 8-8v-53a48.01 48.01 0 10-56 0z"></path>
-                </svg>
-              </div>
-              <div class="flex-grow">
-                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
-                  Seguridad
-                </h2>
-                <p class="leading-relaxed text-base">
-                  No temas por tu
-                  seguridad, esto es un
-                  lugar seguro. Todos
-                  los movimientos que
-                  realices en esta
-                  plataforma son
-                  privados. Además
-                  puedes revisar cómo
-                  son tratados tus
-                  datos.
-                </p>
-                <Link
-                  className="mt-3 text-green-500 inline-flex items-center"
-                  to="/legalterms"
-                >
-                  Términos legales
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </Link>
-              </div>
-            </div>
-            <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-              <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
-                <svg
-                  viewBox="64 64 896 896"
-                  focusable="false"
-                  data-icon="team"
-                  width="3em"
-                  height="3em"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M824.2 699.9a301.55 301.55 0 00-86.4-60.4C783.1 602.8 812 546.8 812 484c0-110.8-92.4-201.7-203.2-200-109.1 1.7-197 90.6-197 200 0 62.8 29 118.8 74.2 155.5a300.95 300.95 0 00-86.4 60.4C345 754.6 314 826.8 312 903.8a8 8 0 008 8.2h56c4.3 0 7.9-3.4 8-7.7 1.9-58 25.4-112.3 66.7-153.5A226.62 226.62 0 01612 684c60.9 0 118.2 23.7 161.3 66.8C814.5 792 838 846.3 840 904.3c.1 4.3 3.7 7.7 8 7.7h56a8 8 0 008-8.2c-2-77-33-149.2-87.8-203.9zM612 612c-34.2 0-66.4-13.3-90.5-37.5a126.86 126.86 0 01-37.5-91.8c.3-32.8 13.4-64.5 36.3-88 24-24.6 56.1-38.3 90.4-38.7 33.9-.3 66.8 12.9 91 36.6 24.8 24.3 38.4 56.8 38.4 91.4 0 34.2-13.3 66.3-37.5 90.5A127.3 127.3 0 01612 612zM361.5 510.4c-.9-8.7-1.4-17.5-1.4-26.4 0-15.9 1.5-31.4 4.3-46.5.7-3.6-1.2-7.3-4.5-8.8-13.6-6.1-26.1-14.5-36.9-25.1a127.54 127.54 0 01-38.7-95.4c.9-32.1 13.8-62.6 36.3-85.6 24.7-25.3 57.9-39.1 93.2-38.7 31.9.3 62.7 12.6 86 34.4 7.9 7.4 14.7 15.6 20.4 24.4 2 3.1 5.9 4.4 9.3 3.2 17.6-6.1 36.2-10.4 55.3-12.4 5.6-.6 8.8-6.6 6.3-11.6-32.5-64.3-98.9-108.7-175.7-109.9-110.9-1.7-203.3 89.2-203.3 199.9 0 62.8 28.9 118.8 74.2 155.5-31.8 14.7-61.1 35-86.5 60.4-54.8 54.7-85.8 126.9-87.8 204a8 8 0 008 8.2h56.1c4.3 0 7.9-3.4 8-7.7 1.9-58 25.4-112.3 66.7-153.5 29.4-29.4 65.4-49.8 104.7-59.7 3.9-1 6.5-4.7 6-8.7z"></path>
-                </svg>
-              </div>
-              <div class="flex-grow">
-                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
-                  Social
-                </h2>
-                <p class="leading-relaxed text-base">
-                  Conéctate todos los
-                  días a la plataforma
-                  para poder charlar con
-                  tus amigos y debatir
-                  la siguiente apuesta
-                  ganadora!
-                </p>
-              </div>
-            </div>
-          </div>
+          </section>
         </div>
-      </section>
-      <Footer />
+        <Footer />
+      </div>
 
       {/*       <pre>
         {JSON.stringify(user, null, 2)}
